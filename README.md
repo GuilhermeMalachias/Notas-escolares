3b2364cfe4d010e44d66f57f4d5c73193e0939fe
Guilherme Malachias <gui.malachias@gmail.com>
2020-12-01 19:47:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Notas-escolares
+ É um pequeno código em JavaScript que simula as notas de alunos e mostra se estão ou não reprovados.
---
+++ index.js
@@ -0,0 +1,25 @@
+var nomes =["Guilherme", "Gabriel", "Aline"];
+var notasA =[8.7, 6.5, 9.5];
+var notasB =[9.5, 7.0, 8.5];
+
+function media(n1,n2){
+    return (n1+n2) / 2;
+}
+
+function passou(media){
+
+    if(media >= 7){
+        return "Aprovado!";
+    }else {
+        return "Reprovado";
+    }
+}
+
+for(var index in nomes){
+
+    var nota1 = notasA[index];
+    var nota2 = notasB[index];
+    var m = media(nota1, nota2);
+
+    console.log(`${nomes[index]} - ${nota1} - ${nota2} - ${m} ${passou(m)}`);
+}
